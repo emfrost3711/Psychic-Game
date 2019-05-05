318a2e088c0ad9ed86bd117f4c7be90ffe93d545
Emily Frost <emfrost@gmail.com>
2019-05-05 15:55:00 -0600

    wins losses remaining guesses all functional woot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ console.log("my javaScript is linked")
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-var guessesLeft = 10;
+var remainingGuesses = 9;
 var guessesSoFar = [];
 var wins = 0;
 var losses = 0;
@@ -13,25 +13,27 @@ document.onkeypress = function (event) {
    var userGuess = event.key.toUpperCase();
   if (userGuess === computerChoice) {
       wins++;
+      // remainingGuesses stays the same
       document.getElementById("wins").textContent=wins; 
         console.log("userGuess");
         reset ();
   } else {
-      losses++;
-      document.getElementById("losses").textContent=losses;
+      document.getElementById("remainingGuesses").textContent=remainingGuesses;
       guessesSoFar.push(userGuess); 
       console.log("guessesSoFar" + guessesSoFar);
+      remainingGuesses--;
       document.getElementById("lettersGuessed").textContent=guessesSoFar;
-      console.log("userLost");
-    if (guessesLeft===0) {
-        reset ();
+  }if (remainingGuesses===0) {
+      losses++;
+      document.getElementById("losses").textContent=losses;
+      reset ();
     }
   }
-}
+
 
 function reset () {
     computerChoice=letters[Math.floor(Math.random() * letters.length)];
-    guessesLeft=10;
+    remainingGuesses=9;
     guessesSoFar=[]
     console.log("computerChoice" + computerChoice) 
 }
